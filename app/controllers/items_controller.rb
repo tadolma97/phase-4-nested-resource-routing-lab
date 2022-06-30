5134f545f17ad53b95d2602792b2be9726942b16
Tashi Dolma <tadolma97@gmail.com>
2022-06-30 11:35:17 -0400

    commit tree d83eda1862ade7ffa5651a966ed4d39c8f6105dd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user=User.find(params[:user_id])
+      items=user.items 
+    else 
+      items=Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item=Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user=User.find(params[:user_id])
+    item=user.items.create(items_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+  
+  def render_not_found_response
+    render json: { error: "Not Found" }, status: :not_found 
+  end
+
 
 end
